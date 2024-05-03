8de5b1136f0584ae2e3c9c8d4e1ba7cb818b689c
Dmarzzucco <ksksks>
2024-05-03 17:13:12 -0300

    fresh

--- src/app/components/header-comp/header-comp.component.ts
+++ src/app/components/header-comp/header-comp.component.ts
@@ -1,37 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { FooterCompComponent } from '../footer-comp/footer-comp.component';
+import { LinkNav } from '../share/share-secon/share-secon.component';
 
 @Component({
   selector: 'app-header-comp',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, LinkNav],
   template: `
-   <header class="fixed flex px-2 my-2 flex-col justify-center items-center w-full">
-            <div class="flex px-3 flex-row text-20 justify-between items-center w-full">
-                <a routerLink="/" class="cursor-pointer">
-                    <i class="fa-solid fa-chevron-left" ></i>
-                </a>
-                <button class="cursor-pointer" (click)="handMenu()">
-                  <i class="fa-solid fa-bars-staggered"></i>
-                </button>
-            </div>
-            @if (openMenu === true){
-              <section (click)="closeMenu()"  class='  h-screen w-full bg-black bg-opacity-70 flex flex-col justify-center items-center'> 
-                <a routerLink="/Microsoft">Micro</a>
-                <a routerLink="/Steam">Steam</a>
-               </section> 
+<header class="fixed flex px-2 my-2 flex-col justify-center items-center w-full">
+  @if (openMenu === false){
+    <div class=" flex px-2 my-2 flex-col justify-center items-center w-full">
+              <div class="flex px-3 flex-row text-20 justify-between items-center w-full">
+                  <a routerLink="/" class="cursor-pointer">
+                      <i class="fa-solid fa-chevron-left" ></i>
+                  </a>
+                  <button class="cursor-pointer" (click)="handMenu()">
+                    <i class="fa-solid fa-bars-staggered"></i>
+                  </button>
+              </div>  
+          </div>
 
-            } @else {}
+  } @else{}
+     
+  @if (openMenu === true){
+        <section (click)="closeMenu()"  class='  h-screen w-full bg-black bg-opacity-70 flex flex-col justify-center items-center'> 
+          <LinkNav [title]="'Steam'" [RouterLink]="'/Steam'" [Icon]="'fa-brands fa-steam'"/>
+          <LinkNav [title]="'Microsoft'" [RouterLink]="'/Microsoft'" [Icon]="'fa-brands fa-microsoft'"/>
+         </section> 
+      } @else {}
 
-        </header>
+  
+</header>
   `,
 })
 export class HeaderCompComponent {
   openMenu: boolean = false;
 
   handMenu() {
-    this.openMenu =!this.openMenu;
+    this.openMenu = !this.openMenu;
   }
   closeMenu() {
     this.openMenu = false;
--- src/app/components/share/share-secon/share-secon.component.ts
+++ src/app/components/share/share-secon/share-secon.component.ts
@@ -4,7 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'ListHm',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   template: `
         <a [routerLink]="RouterLink" routerLinkActive="active"
             class="flex flex-row justify-start items-center hover:bg-slate-500 hover:text-slate-800 p-2 my-1 rounded-2xl border border-slate-600 w-200">
@@ -16,7 +16,27 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 
 export class ListHm {
   @Input() iconClass?: string;
-  @Input() Child?:string;
-  @Input() RouterLink?:string;
+  @Input() Child?: string;
+  @Input() RouterLink?: string;
+
+}
+
+@Component({
+  selector: 'LinkNav',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  template: `
+   <a [routerLink]="RouterLink" routerLinkActive= "active " 
+   class = 'w-200 flex flex-row justify-start items-center p-2 border rounded-xl my-1 hover:bg-slate-300 hover:text-slate-900'  >
+    <i [class]="Icon" ></i>
+    <p class="mx-2">{{title}}</p>
+   </a>
+  `,
+})
+
+export class LinkNav {
+  @Input() title?: string;
+  @Input() RouterLink?: string;
+  @Input() Icon?: string;
 
 }
