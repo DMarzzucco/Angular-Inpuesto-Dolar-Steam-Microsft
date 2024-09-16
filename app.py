0b39fb28cf417111bbb4382189bd1c8b3f8ac170
darmarzz <123565631+DMarzzucco@users.noreply.github.com>
2024-09-16 12:16:45 -0300

    CRUD is finisher

--- app.py
+++ app.py
@@ -1,23 +1,66 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from product import products
 
 app = Flask(__name__)
 
 
 @app.route("/ping", methods=['GET'])
-def GetProducts():
-    return jsonify({"products": products, "message": "List's Products"})
+def get_products():
+    try:
+        return jsonify({"products": products, "message": "List's Products"})
+    except ValueError:
+        return jsonify(ValueError)
 
 
 @app.route("/ping/<string:product_name>", methods=['GET'])
-def GetProduc(product_name):
-    [product for product in products if product ]
-    return jsonify()
+def get_product(product_name):
+    product_found = [product for product in products if product["name"] == product_name]
+    try:
+        if len(product_found) == 0:
+            return jsonify({"message": "Product not found"})
+        return jsonify({"product": product_found[0]})
+    except ValueError:
+        return jsonify(ValueError)
 
 
-@app.router("/ping", methods=['POST'])
-def PostProducts():
-    return jsonify({"message": "Som"})
+@app.route("/ping", methods=['POST'])
+def add_product():
+    try:
+        new_product = {
+            "name": request.json['name'],
+            "price": request.json['price'],
+            "quantity": request.json['quantity']
+        }
+        products.append(new_product)
+        return jsonify({'message': "product added Succesfully", "products": products})
+    except ValueError:
+        return jsonify(ValueError)
+
+
+@app.route("/ping/<string:product_name>", methods=['PUT'])
+def edit_product(product_name):
+    product_found = [product for product in products if product['name'] == product_name]
+    try:
+        if len(product_found) > 0:
+            product_found[0]['name'] = request.json['name']
+            product_found[0]['price'] = request.json['price']
+            product_found[0]['quantity'] = request.json['quantity']
+            return jsonify({"message": "Product updated", "product": product_found[0]})
+        return jsonify({"message": "product not found "})
+    except ValueError:
+        return jsonify(ValueError)
+
+
+@app.route("/ping/<string:product_name>", methods=['DELETE'])
+def delete_product(product_name):
+    product_found = [product for product in products if product['name'] == product_name]
+    try:
+        if len(product_found) == 0:
+            return jsonify({"message": "product not found"})
+        products.remove(product_found[0])
+        return jsonify({"message": "product was deleted"})
+    except ValueError:
+        return jsonify(ValueError)
 
 
 if __name__ == "__main__":
